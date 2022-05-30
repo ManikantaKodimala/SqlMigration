592ec35d420e1ee62a4bffe41b7945f5b6c54a91
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-05-30 10:58:48 +0530

    add teams table

--- models/model.py
+++ models/model.py
@@ -4,6 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+class Teams(Base):
+    __tablename__ = 'teams'
+    id = Column(Integer, primary_key=True)
+    team_name = Column(String)
+
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
